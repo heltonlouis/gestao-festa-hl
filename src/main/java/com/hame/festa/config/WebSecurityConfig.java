2c18e6a515cffa41aeaa74ec5ad3109c402cd53f
Helton Luis <heltonlouis@pop-os.localdomain>
2022-04-29 13:42:45 -0300

    Implementando Spring Security

--- src/main/java/com/hame/festa/config/WebSecurityConfig.java
+++ src/main/java/com/hame/festa/config/WebSecurityConfig.java
@@ -38,6 +38,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/resources/**");
+        web.ignoring().antMatchers("/resources/**", "/static/**");
     }
+    
 }
--- src/main/java/com/hame/festa/controller/UsuarioController.java
+++ src/main/java/com/hame/festa/controller/UsuarioController.java
@@ -1,7 +1,11 @@
 package com.hame.festa.controller;
 
 import com.hame.festa.model.Usuario;
+import com.hame.festa.repository.UsuarioRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,10 +13,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/usuarios")
 public class UsuarioController {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
     
     @RequestMapping("")
     public String usuarios(Usuario usuario) {
-        System.out.println("Usuario: " + usuario.getNome());
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (principal instanceof UserDetails) {
+            String username = ((UserDetails)principal).getUsername();
+            
+            usuario = usuarioRepository.findByLogin(username);
+        }
+        System.out.println("Usuario: " + usuario.getSenha());
         return "usuarios";
     }
 }
